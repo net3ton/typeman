590be3b6d371114df632a81182de2158c39555a9
net7ton <net7ton@gmail.com>
2018-04-22 19:44:58 +0300

    new command show

--- main/exit.go
+++ main/exit.go
@@ -80,23 +80,3 @@ embedded_components {
     w: 1.0
   }
 }
-embedded_components {
-  id: "sign"
-  type: "sprite"
-  data: "tile_set: \"/images/main.atlas\"\n"
-  "default_animation: \"typeman_sign\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 0.0
-    y: 12.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
