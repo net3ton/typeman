93fcd5091de6a10de94d5692c5c39eaf7a31bc2b
net7ton <net7ton@gmail.com>
2018-04-23 14:21:18 +0300

    fixed command entering for html5 version

--- main/console.go
+++ main/console.go
@@ -67,7 +67,7 @@ embedded_components {
   "blend_mode: BLEND_MODE_ALPHA\n"
   "line_break: false\n"
   "text: \"cmd:\"\n"
-  "font: \"/fonts/system_font.font\"\n"
+  "font: \"/builtins/fonts/system_font.font\"\n"
   "material: \"/builtins/fonts/label.material\"\n"
   ""
   position {
