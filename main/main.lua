f621f07590ddf8911c7f5c2ba4cddbc38461df3b
net7ton <net7ton@gmail.com>
2018-05-02 22:54:15 +0300

    moved to global command pool in module

--- main/main.lua
+++ main/main.lua
@@ -1,9 +1,40 @@
 local Main = {}
 
 Main.version = 0
+Main.pool = {}
+--Main.pool_v1 = { jump = true, turn = true, getdoor = true, killboss = true }
 
 function Main.inc_version()
 	Main.version = Main.version + 1
+end
+
+function Main.add_new_command(cmdhash)
+	Main.pool[cmdhash] = true
+end
+
+function Main.has_command(cmd)
+	return Main.pool[hash(cmd)]
+end
+
+--function Main.go_to_ver1()
+--	Main.add_new_command(hash("jump"))
+--	Main.add_new_command(hash("turn"))
+--end
+
+function Main.get_command_name(cmdhash)
+	if cmdhash == hash("jump") then
+		return "jump"
+	elseif cmdhash == hash("turn") then
+		return "turn"
+	elseif cmdhash == hash("getdoor") then
+		return "get my door back"
+	elseif cmdhash == hash("killboss") then
+		return "finish him or her"
+	--elseif self.command == hash("win") then
+	--	return "win this awesome game"
+	end
+
+	return "jump"
 end
 
 return Main
